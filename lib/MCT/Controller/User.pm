d44346d037f4595b3309b52fd42b363c3b87126e
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-02-26 19:11:54 +0100

    Add login form

--- lib/MCT/Controller/User.pm
+++ lib/MCT/Controller/User.pm
@@ -2,6 +2,38 @@ package MCT::Controller::User;
 
 use Mojo::Base 'Mojolicious::Controller';
 
+sub login {
+  my $c = shift;
+  my $validation = $c->validation;
+  my $user = $c->model->user(id => $c->session('uid'));
+  my $update = $c->req->method eq 'POST';
+
+  if (!$user->id) {
+    return $c->redirect_to('login');
+  }
+  if ($update and $user->validate($c->validation)->has_error) {
+    return;
+  }
+
+  $c->delay(
+    sub {
+      my ($delay) = @_;
+      return $user->save($validation->output, $delay->begin) if $update;
+      return $user->load($delay->begin);
+    },
+    sub {
+      my ($delay, $err) = @_;
+      return $c->reply->exception($err) if $err; # TODO: Better error handling
+      return $c->session(uid => 0)->redirect_to('register') unless $user->id; # TODO: Is this even possible?
+      return $c->respond_to(
+        json => {json => $user},
+        any => sub {shift->render(user => $user)}
+      );
+    },
+  );
+
+}
+
 sub profile {
   my $c = shift;
   my $validation = $c->validation;
@@ -52,7 +84,7 @@ sub register {
     },
     sub {
       my ($delay, $err) = @_;
-      return $c->reply->exception($err) if $err; # TODO: Better error handling
+      return $c->render(error => $err) if $err; # TODO: Better error handling
       return $c->session(uid => $user->id)->redirect_to('profile');
     },
   );

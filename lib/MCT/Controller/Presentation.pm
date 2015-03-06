6d04081474035d951d82d332794198f938ff56b1
Joel Berger <joel.a.berger@gmail.com>
2015-03-05 21:19:48 -0600

    controller method for saving a presentation

--- lib/MCT/Controller/Presentation.pm
+++ lib/MCT/Controller/Presentation.pm
@@ -37,6 +37,30 @@ sub update {
 sub save {
   my $c = shift;
 
+  my $title  = $c->param('title');
+  my $p = $c->model->presentation(
+    abstract   => $c->param('abstract'),
+    author     => $c->session('username'),
+    conference => $c->stash('conference')->identifier,
+    subtitle   => $c->param('subtitle'),
+    title      => $title,
+    url_name   => $c->stash('url_name') || $c->param('url_name') || $c->_url_name($title),
+  );
+  $c->delay(
+    sub { $p->save(shift->begin) },
+    sub {
+      my ($delay, $err) = @_;
+      die $err if $err;
+      $c->redirect_to(presentation => url_name => $p->url_name);
+    },
+  );
+}
+
+sub _url_name {
+  my ($c, $title) = @_;
+  $title =~ s/\s/_/g;
+  $title =~ s/\W//g;
+  return $title;
 }
 
 1;

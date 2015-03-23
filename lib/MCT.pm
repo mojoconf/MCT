459914cdccdf1a8e3c132c67b1e9913b0fe53962
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-03-23 18:09:34 +0100

    Fix loading fonts over https

--- lib/MCT.pm
+++ lib/MCT.pm
@@ -52,9 +52,9 @@ sub _assets {
   );
 
   $app->asset('mojoconf.css' => (
-    'http://fonts.googleapis.com/css?family=Oswald:400,300,700',
-    'http://fonts.googleapis.com/css?family=PT+Sans+Narrow',
-    'http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
+    'https://fonts.googleapis.com/css?family=Oswald:400,300,700',
+    'https://fonts.googleapis.com/css?family=PT+Sans+Narrow',
+    'https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
     '/sass/mct.scss',
   ));
 }

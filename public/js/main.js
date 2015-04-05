0999361636910ab1853c4aa4e855a4f01d278b2c
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-04-05 20:41:39 +0200

    Add $.scrollToContent()

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,12 @@
 ;(function($) {
-  $(document).ready(function() {
+  $.scrollToContent = function() {
     var referrer = $('body').attr('data-referrer');
     if (referrer && location.host.indexOf(referrer) == 0 && $('body').scrollTop() < 40) {
       $('html, body').animate({
         scrollTop: $('.content-wrapper').offset().top - 40
       }, 100);
     }
-  });
+  };
+
+  $(document).ready(function() { $.scrollToContent(); });
 })(jQuery);
